35c0316f66a0f67f27f0f00e957fea3f158f52fb
mauricia-10 <rimie.nkou.20@neoma-bs.com>
2021-12-07 11:18:46 +0100

    Quest 4

--- assets/app.js
+++ assets/app.js
@@ -11,15 +11,4 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
-const $ = require('bootstrap');
-// the bootstrap module doesn't export/return anything
-
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
-
 console.log('Hello Webpack Encore !')
